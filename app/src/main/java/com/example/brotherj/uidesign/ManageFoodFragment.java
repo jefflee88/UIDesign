c5091c6f89de9527ccb2b0559df478d9f9c82293
jefflee88 <jeffersonlee88@gmail.com>
2016-03-17 01:48:52 +0800

    1231231

--- app/src/main/java/com/example/brotherj/uidesign/ManageFoodFragment.java
+++ app/src/main/java/com/example/brotherj/uidesign/ManageFoodFragment.java
@@ -3,9 +3,14 @@ package com.example.brotherj.uidesign;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -22,8 +27,27 @@ public class ManageFoodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_manage_food, container, false);
+
+        ListView lstSearchResult = (ListView)view.findViewById(R.id.lstSearchResult);
+        String[] testList = new String[] {"test 1", "test 2", "test 3",  "test 4",  "test 5"};
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, testList);
+
+        lstSearchResult.setAdapter(adapter);
+        lstSearchResult.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                ModifyFoodFragment fragment = new ModifyFoodFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 
 }
