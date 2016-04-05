8878a99e9dd75b46c29100b7e66ab2f27d1b518a
kocc10 <kocc1183@gmail.com>
2016-04-06 05:02:16 +0800

    asd
    asd

--- app/src/main/java/com/example/brotherj/uidesign/ModifyFoodFragment.java
+++ app/src/main/java/com/example/brotherj/uidesign/ModifyFoodFragment.java
@@ -2,6 +2,7 @@ package com.example.brotherj.uidesign;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,8 +22,7 @@ import com.example.brotherj.uidesign.JsonClass.GetJson;
  */
 public class ModifyFoodFragment extends Fragment {
     TextView txtModifyFoodId;
-    EditText edtModifyFoodName;
-    EditText edtModifyFoodPrice;
+    EditText edtModifyFoodName,edtModifyFoodPrice;
 
 
     public ModifyFoodFragment() {
@@ -33,33 +33,28 @@ public class ModifyFoodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_manage_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_modify_food, container, false);
         txtModifyFoodId = (TextView) view.findViewById(R.id.txtModifyFoodId);
         edtModifyFoodName = (EditText) view.findViewById(R.id.edtModifyFoodName);
         edtModifyFoodPrice = (EditText) view.findViewById(R.id.edtModifyFoodPrice);
-        //Button btnModifySave = (Button) view.findViewById(R.id.btnModifySave);
-        //btnModifySave.setOnClickListener(new View.OnClickListener() {
-            //public void onClick(View view) {
+        txtModifyFoodId.setText(SaveData.searchFood.getId());
+        edtModifyFoodName.setText(SaveData.searchFood.getName());
+        edtModifyFoodPrice.setText(SaveData.searchFood.getPrice());
+        Button btnModifySave = (Button) view.findViewById(R.id.btnModifySave);
+        btnModifySave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                SaveData.ModifyFood = SaveData.searchFood;
+                SaveData.ModifyFood.setName(edtModifyFoodName.getText().toString());
+                SaveData.ModifyFood.setPrice(edtModifyFoodPrice.getText().toString());
+                
 
-            //}
-        //});
+            }
+        });
         return view;
 
 
     }
-    @Override
-    public void onStart(){
-        super.onStart();
-        txtModifyFoodId.setText(SaveData.searchFood.getId());
-        edtModifyFoodName.setText(SaveData.searchFood.getName());
-        edtModifyFoodPrice.setText(SaveData.searchFood.getPrice());
 
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-    }
 }
 
 
