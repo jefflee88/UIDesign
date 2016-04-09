8d8cb99ee993e9999e38c9f80011ee40b3abb351
kocc10 <kocc1183@gmail.com>
2016-04-10 01:52:09 +0800

    Add food Ko

--- app/src/main/java/com/example/brotherj/uidesign/AddFoodFragment.java
+++ app/src/main/java/com/example/brotherj/uidesign/AddFoodFragment.java
@@ -6,12 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.brotherj.uidesign.Data.SaveData;
+import com.example.brotherj.uidesign.JsonClass.GetJson;
+import com.example.brotherj.uidesign.bean.Food;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AddFoodFragment extends Fragment {
+    EditText edtModifyFoodName,edtModifyFoodPrice;
+    Spinner spinType;
 
 
     public AddFoodFragment() {
@@ -22,8 +33,27 @@ public class AddFoodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_food, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_food, container, false);
+        GetJson.changeVersion();
+        edtModifyFoodName = (EditText) view.findViewById(R.id.edtAddFoodName);
+        edtModifyFoodPrice = (EditText) view.findViewById(R.id.edtAddFoodPrice);
+        spinType=(Spinner) view.findViewById(R.id.spinType);
+        String[] items={ "french fries", "chicken", "drink", "food", "dessert"};
+        ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, items);
+        LTRadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinType.setAdapter(LTRadapter);
+
+        Button btnModifySave = (Button) view.findViewById(R.id.btnAddAddFood);
+        btnModifySave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Food modifyFood = new Food("null","null","null","null","null","null");
+                modifyFood.setName(edtModifyFoodName.getText().toString());
+                modifyFood.setPrice(edtModifyFoodPrice.getText().toString());
+                modifyFood.setType(spinType.getSelectedItem().toString());
+                GetJson.addFood(modifyFood);
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/example/brotherj/uidesign/JsonClass/GetJson.java
+++ app/src/main/java/com/example/brotherj/uidesign/JsonClass/GetJson.java
@@ -51,8 +51,8 @@ public class GetJson {
             String reply = result.toString();
             JSONObject json = new JSONObject(reply);
             try {
-                if(type.equals("customer")){
-                    for(int i=0; i<json.getJSONArray("user").length();i++) {
+                if (type.equals("customer")) {
+                    for (int i = 0; i < json.getJSONArray("user").length(); i++) {
                         JSONObject jsonObj = json.getJSONArray("user").getJSONObject(i);
                         String id = jsonObj.getString("id");
                         String name = jsonObj.getString("name");
@@ -60,10 +60,10 @@ public class GetJson {
                         String email = jsonObj.getString("email");
                         String telNum = jsonObj.getString("telNum");
                         String Userid = jsonObj.getString("Userid");
-                        SaveData.customer=new Customer(id, name, address, email, telNum, Userid);
+                        SaveData.customer = new Customer(id, name, address, email, telNum, Userid);
                     }
                 }
-                if(type.equals("restaurant")) {
+                if (type.equals("restaurant")) {
                     for (int i = 0; i < json.getJSONArray("order").length(); i++) {
                         JSONObject jsonObj = json.getJSONArray("order").getJSONObject(i);
                         String id = jsonObj.getString("id");
@@ -75,14 +75,14 @@ public class GetJson {
                         SaveData.restaurant = new Restaurant(id, name, address, type1, telNum, Userid);
                     }
                 }
-                if(type.equals("driver")){
-                    for(int i=0; i<json.getJSONArray("order").length();i++) {
+                if (type.equals("driver")) {
+                    for (int i = 0; i < json.getJSONArray("order").length(); i++) {
                         JSONObject jsonObj = json.getJSONArray("order").getJSONObject(i);
                         String id = jsonObj.getString("id");
                         String name = jsonObj.getString("name");
                         String telNum = jsonObj.getString("telNum");
                         String Userid = jsonObj.getString("Userid");
-                        SaveData.driver=new Driver(id, name, telNum, Userid);
+                        SaveData.driver = new Driver(id, name, telNum, Userid);
                     }
 
                 }
@@ -129,7 +129,7 @@ public class GetJson {
         return check;
     }
 
-    public static String CheckUserType(String userId,String userPw) {
+    public static String CheckUserType(String userId, String userPw) {
         String type = "";
         try {
             String url = "http://10.0.2.2/fyp_connect/get_user_details.php?id=" + userId + "&password=" + userPw;
@@ -169,7 +169,7 @@ public class GetJson {
     public static ArrayList<Food> getRestFoodDetail(String userId) {
         ArrayList<Food> food = new ArrayList<Food>();
         try {
-            String url = "http://10.0.2.2/fyp_connect/get_restaurant_food.php?userid="+userId;
+            String url = "http://10.0.2.2/fyp_connect/get_restaurant_food.php?userid=" + userId;
             URL urlObj = new URL(url);
             HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
             client.setDoInput(true);
@@ -186,48 +186,7 @@ public class GetJson {
             String reply = result.toString();
             JSONObject json = new JSONObject(reply);
             try {
-                    for(int i=0; i<json.getJSONArray("food").length();i++) {
-                        JSONObject jsonObj = json.getJSONArray("food").getJSONObject(i);
-                        String foodId = jsonObj.getString("id");
-                        String foodName = jsonObj.getString("name");
-                        String foodType = jsonObj.getString("type");
-                        String foodPrice = jsonObj.getString("price");
-                        String foodImage = jsonObj.getString("image");
-                        String foodRestId = jsonObj.getString("Restaurantid");
-                        food.add(new Food(foodId, foodName, foodType, foodPrice, foodImage, foodRestId));
-                    }
-
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
-
-        } catch (Exception e1) {
-            e1.printStackTrace();
-        }
-        return food;
-    }
-
-    public static ArrayList<Food> searchFood(String keyword,String type){
-        ArrayList<Food> food = new ArrayList<Food>();
-        try {
-            String url = "http://10.0.2.2/fyp_connect/search_details.php?type="+type+"&keyword="+keyword;
-            URL urlObj = new URL(url);
-            HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
-            client.setDoInput(true);
-            client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-            client.setRequestMethod("GET");
-            client.connect();
-            InputStream input = client.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            String reply = result.toString();
-            JSONObject json = new JSONObject(reply);
-            try {
-                for(int i=0; i<json.getJSONArray("food").length();i++) {
+                for (int i = 0; i < json.getJSONArray("food").length(); i++) {
                     JSONObject jsonObj = json.getJSONArray("food").getJSONObject(i);
                     String foodId = jsonObj.getString("id");
                     String foodName = jsonObj.getString("name");
@@ -248,9 +207,10 @@ public class GetJson {
         return food;
     }
 
-    public static void modifyFood(Food food){
+    public static ArrayList<Food> searchFood(String keyword, String type) {
+        ArrayList<Food> food = new ArrayList<Food>();
         try {
-            String url = "http://10.0.2.2/fyp_connect/update_food.php?id="+food.getId()+"&name="+food.getName()+"&type="+food.getType()+"&price="+food.getPrice()+"&image="+food.getImage();
+            String url = "http://10.0.2.2/fyp_connect/search_details.php?type=" + type + "&keyword=" + keyword;
             URL urlObj = new URL(url);
             HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
             client.setDoInput(true);
@@ -266,9 +226,70 @@ public class GetJson {
             }
             String reply = result.toString();
             JSONObject json = new JSONObject(reply);
+            try {
+                for (int i = 0; i < json.getJSONArray("food").length(); i++) {
+                    JSONObject jsonObj = json.getJSONArray("food").getJSONObject(i);
+                    String foodId = jsonObj.getString("id");
+                    String foodName = jsonObj.getString("name");
+                    String foodType = jsonObj.getString("type");
+                    String foodPrice = jsonObj.getString("price");
+                    String foodImage = jsonObj.getString("image");
+                    String foodRestId = jsonObj.getString("Restaurantid");
+                    food.add(new Food(foodId, foodName, foodType, foodPrice, foodImage, foodRestId));
+                }
+
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return food;
+    }
+
+    public static void modifyFood(Food food) {
+        try {
+            String url = "http://10.0.2.2/fyp_connect/update_food.php?id=" + food.getId() + "&name=" + food.getName() + "&type=" + food.getType() + "&price=" + food.getPrice() + "&image=" + food.getImage();
+            url = url.replaceAll(" ","%20");
+            Log.d("Url   :::::::", url);
+            URL urlObj = new URL(url);
+            HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
+            client.setDoInput(true);
+            client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            client.setRequestMethod("GET");
+            client.connect();
+            InputStream input = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
         } catch (Exception e1) {
             e1.printStackTrace();
         }
     }
 
+    public static void addFood(Food food) {
+        try {
+            String url = "http://10.0.2.2/fyp_connect/create_food.php?name=" + food.getName() + "&type=" + food.getType() + "&price=" + food.getPrice() + "&image=" + food.getImage()+"&Restaurantid="+SaveData.restaurant.getId();
+            url = url.replaceAll(" ","%20");
+            URL urlObj = new URL(url);
+            HttpURLConnection client = (HttpURLConnection) urlObj.openConnection();
+            client.setDoInput(true);
+            client.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            client.setRequestMethod("GET");
+            client.connect();
+            InputStream input = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
 }
