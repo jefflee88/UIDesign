52b742398ec0864a2898d5ce55030f314d40314e
wilsonwth1938 <tszho1938@yahoo.com.hk>
2016-04-24 03:15:39 +0800

    Map

--- app/src/main/java/com/example/brotherj/uidesign/GetJobDetailFragment.java
+++ app/src/main/java/com/example/brotherj/uidesign/GetJobDetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.brotherj.uidesign;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 
 import com.example.brotherj.uidesign.R;
 
@@ -17,6 +19,8 @@ import com.example.brotherj.uidesign.R;
  */
 public class GetJobDetailFragment extends Fragment {
 
+    ImageButton imgBtnRest;
+    ImageButton imgBtnCust;
 
     public GetJobDetailFragment() {
         // Required empty public constructor
@@ -27,7 +31,19 @@ public class GetJobDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_job_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_get_job_detail, container, false);
+        imgBtnRest = (ImageButton)view.findViewById(R.id.imgBtnRest);
+        imgBtnCust = (ImageButton)view.findViewById(R.id.imgBtnCust);
+
+        imgBtnRest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent Intent = new Intent(view.getContext(), MapsActivity.class);
+                startActivityForResult(Intent, 0);
+            }
+        });
+
+
+        return view;
     }
 
 }
